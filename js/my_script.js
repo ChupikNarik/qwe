f6fb28da7e1aa30034d0d5f5ec4568bbbdf9cdbe
ChupikNarik <chupiknarik@gmail.com>
2016-07-07 20:06:33 +0300

    1

---
+++ js/my_script.js
@@ -0,0 +1,14 @@
+$().ready(function()
+{
+
+    $('#toggle_click').click(function()
+    {
+        if ($(this).hasClass('opened')) {
+            $('#toggle_menu').fadeOut('normal');
+            $(this).removeClass('opened');
+        }else{
+            $('#toggle_menu').fadeIn('normal');
+            $(this).addClass('opened');
+        }
+    });
+});
